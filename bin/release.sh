949b31bc984ed65040ab009abc1e028056e4c07e
monsterkodi <monsterkodi@gmx.net>
2017-03-10 01:00:33 +0100

    release

---
+++ bin/release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd `dirname $0`/..
+
+APP=`sds -rp productName`
+VERSION=`sds -rp version`
+VVERSION=v$VERSION
+USER=`sds -rp author`
+DMG=$APP-$VERSION.dmg
+
+./bin/github-release release -s $GH_TOKEN -u $USER -r $APP -t $VVERSION -n $VVERSION --pre-release
+./bin/github-release upload  -s $GH_TOKEN -u $USER -r $APP -t $VVERSION -n $DMG -f $DMG
+
