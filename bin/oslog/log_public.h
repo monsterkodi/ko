60218d05ab0a024ff13d404fdec46ff76bd203f3
monsterkodi <monsterkodi@gmx.net>
2017-03-15 00:22:05 +0100

    clean

--- bin/oslog/log_public.h
+++
@@ -1,9 +0,0 @@
-#ifndef log_public_h
-#define log_public_h
-
-#import <Foundation/Foundation.h>
-#import <os/log.h>
-
-void log_public(os_log_t, os_log_type_t, NSString *);
-
-#endif /* log_public_h */
--- bin/oslog/log_public.m
+++
@@ -1,8 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import "log_public.h"
-
-void log_public(os_log_t log, os_log_type_t type, NSString * message)
-{
-    os_log_with_type(log, type, "%{public}s", [message cStringUsingEncoding:NSUTF8StringEncoding]);
-}
