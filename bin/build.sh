2283cd5684b92b7cc45fdb9c1da84ea14d7f8060
monsterkodi <monsterkodi@gmx.net>
2017-05-01 02:34:36 +0200

    pug

--- bin/build.sh
+++ bin/build.sh
@@ -9,7 +9,7 @@ NAME=`sds productName`
 konrad --run
 node_modules/.bin/electron-rebuild
 
-IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|pug$|styl$|package\.noon$|.*\.lock$|three/examples)"
+IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|styl$|package\.noon$|.*\.lock$|three/examples)"
 node_modules/electron-packager/cli.js . --overwrite --icon=img/$NAME.icns --ignore $IGNORE --extend-info ./bin/info.plist --extra-resource ./img/file.icns
 
 cp bin/snap.svg.js $NAME-darwin-x64/ko.app/Contents/Resources/app/js/area/voronoi      
