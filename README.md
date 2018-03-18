86f9bd646802120b6445d18f80235bca98dae9dd
monsterkodi <monsterkodi@gmx.net>
2018-03-18 02:29:44 +0100

    readme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ![ko](img/banner.png)
 
 ![readme](img/readme.png)
-
-<br><br>
-
-**ko** is in active development, use at your own risk!
--- bin/buildmac.sh
+++ bin/buildmac.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 cd `dirname $0`/..
 
-NAME=`sds productName`
+2>/dev/null 1>/dev/null killall ko
+2>/dev/null 1>/dev/null killall ko
 
-2>/dev/null 1>/dev/null killall $NAME
-2>/dev/null 1>/dev/null killall $NAME
+if rm -rf ko-win32-x64; then
 
-konrad --run
-node_modules/.bin/electron-rebuild
-
-IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|styl$|package\.noon$|.*\.lock$|three/examples)"
-node_modules/electron-packager/cli.js . --overwrite --icon=img/$NAME.icns --ignore $IGNORE --extend-info ./bin/info.plist --extra-resource ./img/file.icns
-
-cp bin/snap.svg.js $NAME-darwin-x64/ko.app/Contents/Resources/app/js/stage/voronoi      
-cp bin/voronoinet.js $NAME-darwin-x64/ko.app/Contents/Resources/app/js/stage/voronoi
-
-rm $NAME-darwin-x64/LICENSE*
-rm $NAME-darwin-x64/version
-
+    konrad --run
+    node_modules/.bin/electron-rebuild
+    
+    IGNORE="/(.*\.dmg$|Icon$|coffee$|.*md$|styl$|package\.noon$|.*\.lock$|three/examples)"
+    node_modules/electron-packager/cli.js . --overwrite --icon=img/ko.icns --ignore $IGNORE --extend-info ./bin/info.plist --extra-resource ./img/file.icns
+    
+    rm ko-darwin-x64/LICENSE*
+    rm ko-darwin-x64/version
+    
+fi
