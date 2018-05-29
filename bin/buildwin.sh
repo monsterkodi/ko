b4b3c08e6b46a8c9915d997499129621798e8bcb
monsterkodi <monsterkodi@gmx.net>
2018-05-29 16:12:35 +0200

    misc

--- bin/buildwin.sh
+++ bin/buildwin.sh
@@ -8,7 +8,6 @@ if rm -rf ko-win32-x64; then
     node_modules/.bin/electron-rebuild
     
     IGNORE="(.*\.dmg$|Icon$|.*md$|/coffee$|/inno$|/pug$|/styl$|package\.noon$|.*\.lock$|three/examples)"
-    # node_modules/electron-packager/cli.js . --overwrite --icon=img/ko.ico --no-prune --ignore $IGNORE
     node_modules/electron-packager/cli.js . --overwrite --icon=img/ko.ico --ignore $IGNORE
     
 fi    
